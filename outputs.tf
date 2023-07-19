3361e169aedb71cb0a80cd11f231f200b4f7a08d
Siphephelo Mlungwana <siphephelo.mlungwana@splitpoint.co.za>
2023-07-19 08:55:40 +0200

    edited outputs.ft file to output address of EIP

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "mivu_grafana" {
   description = "This is the public IP of MiVu Server"
-  value       = "http://${aws_instance.mivu_server.public_ip}:3000"
+  value       = "http://${aws_eip.server_eip.public_ip}:3000"
 }
 
 output "mivu_chronograf" {
   description = "This is the public IP of MiVu Server"
-  value       = "http://${aws_instance.mivu_server.public_ip}:8888"
+  value       = "http://${aws_eip.server_eip.public_ip}:8888"
 }
